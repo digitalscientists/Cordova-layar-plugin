fcc93a79b205a489316c0da714223e7c4491e351
Sergey Popenko <SergeyPopenko@gmail.com>
2013-10-07 16:00:35 +0300

    Created intial structure

---
+++ src/ios/CDVLayar.h
@@ -0,0 +1,13 @@
+//
+//  CDVLayar.h
+//  AMC
+//
+//  Created by Sergey Popenko on 10/7/13.
+//
+//
+
+#import <Cordova/Cordova.h>
+
+@interface CDVLayar : CDVPlugin
+
+@end
---
+++ src/ios/CDVLayar.m
@@ -0,0 +1,13 @@
+//
+//  CDVLayar.m
+//  AMC
+//
+//  Created by Sergey Popenko on 10/7/13.
+//
+//
+
+#import "CDVLayar.h"
+
+@implementation CDVLayar
+
+@end
