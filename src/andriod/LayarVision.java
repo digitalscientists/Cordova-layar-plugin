c263d653d3a8fd70e24c3dcaf41855e663c52c04
Anton Rymarchuk <ae.ra.box@gmail.com>
2013-10-09 17:49:31 +0300

    added  android java implementation

---
+++ src/andriod/LayarVision.java
@@ -0,0 +1,46 @@
+package com.ds.cordova.layar;
+
+import java.text.MessageFormat;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.app.Activity;
+import android.util.Log;
+
+import com.layar.sdk.LayarSDKListener;
+import com.layar.sdk.LayarVisionSDK;
+
+public class LayarVision extends CordovaPlugin implements LayarSDKListener {
+
+	private static final String	TAG	= LayarVision.class.getSimpleName();
+
+	@Override
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+		launch();
+		return true;
+	}
+
+	public void launch() {
+		String oauthKey = "IFfARbQzwDoNMVHB";
+		String oauthSecret = "JVMjrbiogcIYKpGNqBReWLnAfHZdzQsl";
+
+		final Activity activity = cordova.getActivity();
+		LayarVisionSDK.initialize(activity, oauthKey, oauthSecret);
+		LayarVisionSDK.startLayarVisionActivity(activity, this);
+	}
+
+	@Override
+	public void onLayerLaunched(String arg0, String arg1) {
+		Log.d(TAG, MessageFormat.format("#onLayerLaunched\narg0 = {0}; arg1 = {1};", arg0, arg1));
+		
+	}
+
+	@Override
+	public void onReferenceImageTracked(String arg0, String arg1, String arg2) {
+		Log.d(TAG, MessageFormat.format("#onReferenceImageTracked\narg0 = {0}; arg1 = {1}; arg2 = {2}", arg0, arg1, arg2));
+	}
+
+}
