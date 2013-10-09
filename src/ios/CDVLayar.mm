b43144327b2eb6f1587d26d6733964a2378373da
Sergey Popenko <SergeyPopenko@gmail.com>
2013-10-09 17:16:47 +0300

    updated iOS plugin

--- src/ios/CDVLayar.mm
+++ src/ios/CDVLayar.mm
@@ -6,8 +6,32 @@
 //
 //
 
-#import "CDVLayar.h"
+#import "CDVLayar.hpp"
+#import <LayarSDK/LayarPlayer.hpp>
 
 @implementation CDVLayar
+
+- (void)pluginInitialize
+{
+    [super pluginInitialize];
+}
+
+
+- (void)launchLayarVision:(CDVInvokedUrlCommand*)command
+{
+    
+    NSString *consumerKey = @"IFfARbQzwDoNMVHB";
+    NSString *consumerSecret = @"JVMjrbiogcIYKpGNqBReWLnAfHZdzQsl";
+    LPARVisionViewController *augmentedRealityViewController = [[LPARVisionViewController alloc] init];
+    augmentedRealityViewController.oauthConsumerKey = consumerKey;
+    augmentedRealityViewController.oauthConsumerSecret = consumerSecret;
+    augmentedRealityViewController.delegate = nil;
+    [self.viewController presentViewController:augmentedRealityViewController animated:YES completion:nil];
+    
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Done"];
+
+    
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
 
 @end
