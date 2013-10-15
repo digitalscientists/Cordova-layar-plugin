90252b63fc0896175f89e4a9398aa94b6db69a07
Anton Rymarchuk <ae.ra.box@gmail.com>
2013-10-15 17:49:55 +0300

    improved script that help regenerate R class.

--- tools/RegeneR.scala
+++ tools/RegeneR.scala
@@ -7,10 +7,22 @@ object RegeneR extends App {
   val ClassR = "(.*public static final class )(.*?) \\{".r
   var currClass = ""
   val result = source.getLines map {
+    case "public final class R {" =>
+"""public class R {
+    private static Resources sRes;
+    private static String sPackageName;
+
+    public static void init(Context ctx){
+        sRes = ctx.getResources();
+        sPackageName = ctx.getPackageName();
+    }
+
+"""
     case ClassR(t, n) => currClass = n; s"$t$n {"
-    case Field(t, n) => s"$t$n = $pkg.R.$currClass.$n;"
+    case Field(t, n) => s"""$t$n = sRes.getIdentifier("$n", "$currClass", sPackageName);"""
     case x           => x
   }
+
   
   result foreach println
   source.close()
