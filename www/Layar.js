f798bb6ac19486795be126b76efd0be27e5b5062
Anton Rymarchuk <ae.ra.box@gmail.com>
2013-10-23 12:09:56 +0300

    added hack-style show/hide hooks

--- www/Layar.js
+++ www/Layar.js
@@ -1,3 +1,11 @@
-window.launchLayarVision = function() {
-     cordova.exec(null, null, "Layar", "launchLayarVision", []);
+window.launchLayarVision = function(show, hide) {
+
+	function isFunction(f){
+       return (typeof f !== "undefined") && (typeof f === "function");
+    }
+    
+    if(isFunction(show)) show();
+    setTimeout(function(){cordova.exec(null, null, "Layar", "launchLayarVision", [])}, 1000);
+    if(isFunction(hide)) setTimeout(hide, 3000);
+    
 };
